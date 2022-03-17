24888ffcc592d7b1db6dab2b91c3294f14ae05f0
NekosForLife <85155633+NekosForLife@users.noreply.github.com>
2022-03-17 14:15:23 +0100

    rudimentaere Kommentare Funktion

--- src/secure/script.js
+++ src/secure/script.js
@@ -1,12 +1,13 @@
-var postTest = document.getElementById("post");
+var postTest = document.getElementById("postButton");
 postTest.addEventListener("click", function(){
-    console.log("test")
     var commentBoxValue= document.getElementById("comment-box").value;
-
     var li = document.createElement("li");
     var text = document.createTextNode(commentBoxValue);
     li.appendChild(text);
     document.getElementById("unordered").appendChild(li);
-    console.log(li.toString())
-    console.log("test")
+// Seite 73 Foliensatz
+// post oder put
+// index.js kommentare holen und schicken
+// navigations attribute localhost:8080/mausdetails?id=123456
+// app.get("/mausdetails")
 });
